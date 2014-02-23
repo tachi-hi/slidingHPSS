81124c6fe48930f676f5599aad3675fb962cd24e
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2014-02-23 19:02:58 +0900

    Update twostageHPSS.sh

--- twostageHPSS.sh
+++ twostageHPSS.sh
@@ -2,6 +2,6 @@
 
 # This is an example of two-stage HPSS
 
-./slidingHPSS --input input.wav --block 100 --frame 512  --Hout _H1.wav --Pout _P.wav -Q 0.3 -W 0.3 
-./slidingHPSS --input _H1.wav   --block 100 --frame 4096 --Hout _H.wav  --Pout _V.wav -Q 0.3 -W 0.3 
+./slidingHPSS --input input.wav --block 100 --frame 128  --Hout _H1.wav --Pout _P.wav -Q 0.3 -W 0.3 
+./slidingHPSS --input _H1.wav   --block 100 --frame 8192 --Hout _H.wav  --Pout _V.wav -Q 0.3 -W 0.3 
 
