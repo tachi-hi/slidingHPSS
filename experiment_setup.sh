0f77d38e26e8ed97710b49689e6c4d25935ee6af
Hideyuki Tachibana <tachi-hi@users.noreply.github.com>
2015-06-30 17:39:51 +0900

    sample files for tutorial (they are empty)

--- experiment_setup.sh
+++ experiment_setup.sh
@@ -18,6 +18,15 @@ touch $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/place_here_MIR1K_data; fi
 if [ ! -d $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile ];
 then mkdir $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile; fi
 
+# Example files
+if [ ! -d $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile/ ];
+then 
+touch $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile/Ani_1_01.wav
+touch $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile/Ani_1_02.wav
+touch $YOUR_WORKING_DIRECTORY/MIR-1K_for_MIREX/Wavfile/Ani_1_03.wav
+fi
+
+
 
 cp ./bin/slidingHPSS $YOUR_WORKING_DIRECTORY
 cp -r ./twostageHPSS_scripts/R/* $YOUR_WORKING_DIRECTORY
